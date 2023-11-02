ef79ab0a8da3e231914712096b0b18a2dc3aa063
funny_233 <83470506+funny233-github@users.noreply.github.com>
2023-11-02 13:00:01 +0800

    change run.sh,support custom java path which use environment varible

--- server/run.sh
+++ server/run.sh
@@ -1,1 +1,33 @@
-"java/bin/java" -Xms600M -Xmx10G -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar fabric-server-launch.jar
+echo $MY_JAVA_PATH
+if [ -z $MY_JAVA_PATH ];then
+    path=java
+else 
+    path=$MY_JAVA_PATH/java
+fi
+
+echo ====================
+echo your path is $path
+$path --version
+echo ====================
+
+$path \
+-Xms600M \
+-Xmx10G \
+-XX:+UseG1GC \
+-XX:MaxGCPauseMillis=200 \
+-XX:+UnlockExperimentalVMOptions \
+-XX:+DisableExplicitGC \
+-XX:+AlwaysPreTouch \
+-XX:G1NewSizePercent=30 \
+-XX:G1MaxNewSizePercent=40 \
+-XX:G1HeapRegionSize=8M \
+-XX:G1ReservePercent=20 \
+-XX:G1HeapWastePercent=5 \
+-XX:G1MixedGCCountTarget=4 \
+-XX:InitiatingHeapOccupancyPercent=15 \
+-XX:G1MixedGCLiveThresholdPercent=90 \
+-XX:G1RSetUpdatingPauseTimePercent=5 \
+-XX:SurvivorRatio=32 \
+-XX:+PerfDisableSharedMem \
+-XX:MaxTenuringThreshold=1 \
+-jar fabric-server-launch.jar nogui
